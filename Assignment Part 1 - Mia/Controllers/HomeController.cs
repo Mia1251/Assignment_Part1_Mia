c4b7cb215ef48e10c3fb1b62825cc4f14fcf20d2
Mia1251 <svanteomax@gmail.com>
2022-11-08 19:36:17 +0100

    Add project files.

---
+++ Assignment Part 1 - Mia/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_Part_1___Mia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Assignment Part 1 - Mia/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.Extensions.ObjectPool;
+
+namespace Assignment_Part_1___Mia
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddMvc();
+            builder.Services.AddHttpContextAccessor();
+
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline
+            if (!app.Environment.IsDevelopment());
+            {
+                app.UseExceptionHandler("/Home/Error");
+
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller = Home}/{action = Index}/{id?}"
+                    );
+            });
+
+            app.Run();
+        }
+    }
+}
